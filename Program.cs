77db08f783af598fae433ff65c647fa5355d4f9b
Настя <kulakova_2004@mail.ru>
2024-03-23 11:43:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab2.Model;
+using Lab2.Controller;
+
+namespace Lab2;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DictionaryDatabase dictionaryDatabase = new DictionaryDatabase();
+        IDictionary dictionary = dictionaryDatabase;
+        ApplicationController appController = new ApplicationController(dictionary);
+        appController.Run();
+    }
+}
